775c85de318cf4803422fa1d3d7faccf664290b2
Benncio09 <lorebencini13@outlook.com>
2024-05-23 13:09:23 +0200

    no more commets

--- index.php
+++ index.php
@@ -1,38 +1,27 @@
 <?php
-// Avvia la sessione
 session_start();
 
-// Controlla se la variabile di sessione login non è impostata
 if (!isset($_SESSION['login'])) {
-    // Reindirizza alla pagina di login
     header("Location: ./login/login.html");
-    exit(); // Assicura che lo script si interrompa dopo il reindirizzamento
+    exit();
 }
 
-// Includi il file di connessione al database
 require_once "./lib/connection.php";
 
-// Prendi l'ID dell'utente dalla variabile di sessione
 $user_id = $_SESSION["login"];
 
-// Query per ottenere tutti i dati dell'utente
 $sql = "SELECT * FROM utenti WHERE id = $user_id";
 $result = $conn->query($sql);
 
-// Verifica se la query ha prodotto dei risultati
 if ($result->num_rows > 0) {
-    // Ottieni i dati dell'utente
     $user_data = $result->fetch_assoc();
-    // Stampa i dati dell'utente
     echo "<h2>Dati dell'utente:</h2>";
     echo "<p>Username: " . $user_data['username'] . "</p>";
     echo "<p>Email: " . $user_data['email'] . "</p>";
     echo "    <a href='./login/logout.php'>disconnessione</a>";
-    // Aggiungi altri dati dell'utente se necessario
 } else {
     echo "Nessun dato dell'utente trovato.";
 }
 
-// Chiudi la connessione al database
 $conn->close();
 ?>
--- login/register.php
+++ login/register.php
@@ -2,9 +2,7 @@
 require_once "connection.php";
 session_start();
 
-// Verifica se sono stati inviati i dati del form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Prendi i valori inviati dal form
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
@@ -12,21 +10,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cognome = $_POST['cognome'];
     $eta = $_POST['eta'];
     $classe = $_POST['classe'];
-    
-    // Query per inserire i dati nel database
+
     $sql = "INSERT INTO Utente (username, password, email, nome, cognome, eta, classe) 
             VALUES ('$username', '$password', '$email', '$nome', '$cognome', '$eta', '$classe')";
     
     if ($conn->query($sql) === TRUE) {
-        // Registrazione riuscita, reindirizza alla pagina di login o a un'altra pagina
         header("Location: index.php");
-        exit(); // Assicura che lo script si interrompa dopo il reindirizzamento
+        exit();
     } else {
-        // Errore durante la registrazione
         echo "Errore durante la registrazione: " . $conn->error;
     }
 }
 
-// Chiudi la connessione al database
 $conn->close();
 ?>
