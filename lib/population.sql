704ec463e0fa34ebcf607f803bae4000ee0ac00c
Benncio09 <lorebencini13@outlook.com>
2024-05-23 14:39:18 +0200

    made navbar and population for categories table

---
+++ lib/population.sql
@@ -0,0 +1,4 @@
+INSERT INTO categoria VALUES(1, "telefonia");
+INSERT INTO categoria VALUES(2, "videogiochi");
+INSERT INTO categoria VALUES(3, "informatica");
+INSERT INTO categoria VALUES(4, "libri");
