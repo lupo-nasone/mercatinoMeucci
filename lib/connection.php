ebee6fc90d7f5be6a76f237f637522e4e67fadb7
lupo-nasone <91332021+lupo-nasone@users.noreply.github.com>
2024-05-24 11:14:39 +0200

    connection

--- lib/connection.php
+++ lib/connection.php
@@ -1,9 +1,9 @@
 <?php
 // Parametri di connessione al database
-$servername = "mariadb"; // Modifica con il nome del server del tuo database
+$servername = "localhost"; // Modifica con il nome del server del tuo database
 $username = "root"; // Modifica con il tuo nome utente del database
 $password = ""; // Modifica con la tua password del database
-$dbname = "mercatino"; // Modifica con il nome del tuo database
+$dbname = "my_faginali5cia"; // Modifica con il nome del tuo database
 
 // Connessione al database
 $conn = new mysqli($servername, $username, $password, $dbname);
