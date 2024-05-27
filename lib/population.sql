975b9061ecc3c66c71139afa62f1c01d298b18fd
nakk0 <langonebros2@gmail.com>
2024-05-27 22:37:16 +0200

    fixed population.sql capitalization

--- lib/population.sql
+++ lib/population.sql
@@ -1,4 +1,4 @@
-INSERT INTO categoria VALUES(1, "telefonia");
-INSERT INTO categoria VALUES(2, "videogiochi");
-INSERT INTO categoria VALUES(3, "informatica");
-INSERT INTO categoria VALUES(4, "libri");
+INSERT INTO Categoria VALUES(1, "telefonia");
+INSERT INTO Categoria VALUES(2, "videogiochi");
+INSERT INTO Categoria VALUES(3, "informatica");
+INSERT INTO Categoria VALUES(4, "libri");
