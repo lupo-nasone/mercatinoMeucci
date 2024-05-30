6d7dd5ea228c53ddc9c06a495d2083d12e66a378
Benncio09 <lorebencini13@outlook.com>
2024-05-30 13:10:31 +0200

    annncio

--- addannuncio/aggiungi.php
+++ addannuncio/aggiungi.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //riscritto il codice senza l'immagine per far funzionare per testing
     {
-    $sql = "INSERT INTO Annncio (titolo, descrizione, Categoria_id, Utente_id) 
+    $sql = "INSERT INTO Annucio (titolo, descrizione, Categoria_id, Utente_id) 
             VALUES ('$titolo', '$descrizione', (SELECT id FROM Categoria WHERE nome = '$tipologia'), $utente_id)";
     $result = $conn->query($sql);
     }
