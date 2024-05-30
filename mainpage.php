e24755ac32d6c5f7171f558925103f440ed9deb3
Benncio09 <lorebencini13@outlook.com>
2024-05-30 13:15:37 +0200

    fixed image not showing

--- mainpage.php
+++ mainpage.php
@@ -31,7 +31,7 @@ unset($_SESSION["MSG_good"]);
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img src="../images/pngwing.com.png" width="75" height="50">Mercatino dell'Assunzione</a>
+      <a class="navbar-brand" href="index.php"><img src="./images/pngwing.com.png" width="75" height="50">Mercatino dell'Assunzione</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
